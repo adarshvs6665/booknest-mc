9b9cce3716a23d8c2fc451b35af6acb145f73115
4bh16am <45392298+abhi123vj@users.noreply.github.com>
2024-12-04 03:11:59 +0530

    build

--- client/src/components/books/ts.tsx
+++ client/src/components/books/ts.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+// import React from 'react'
 
-type Props = {}
+// type Props = {}
 
-const ts = (props: Props) => {
+const ts = () => {
   return (
     <div>ts</div>
   )
